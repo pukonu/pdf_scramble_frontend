bb076ad001a4d9c140fdd418975cffa4a4c49acc
Peter Ukonu <pukonu@gmail.com>
2019-02-11 13:19:10 +0000

    initial commit

---
+++ build/precache-manifest.008695223240d2a0cd7763b07bf0fd80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94b4c851c9943c833e81",
+    "url": "/static/js/main.94b4c851.chunk.js"
+  },
+  {
+    "revision": "384c3bf5e7ccfd5090c3",
+    "url": "/static/js/1.384c3bf5.chunk.js"
+  },
+  {
+    "revision": "94b4c851c9943c833e81",
+    "url": "/static/css/main.c7203a80.chunk.css"
+  },
+  {
+    "revision": "384c3bf5e7ccfd5090c3",
+    "url": "/static/css/1.34c4afd1.chunk.css"
+  },
+  {
+    "revision": "fbc286ce5557fb865afd45297866b3d2",
+    "url": "/index.html"
+  }
+];
